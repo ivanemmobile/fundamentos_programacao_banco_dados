1a714c910c1f4dcd48e24332e24f16af9697af86
Ivan Paulino <ivanemmobile@hotmail.com>
2024-07-16 21:24:41 -0300

    atividade- hellen

---
+++ funcao-hwllwn.py
@@ -0,0 +1,20 @@
+def mulher():
+    print('Alta')
+    print('Baixa')
+    print('Loira')
+    print('Morena')
+m1=str(input('Você é alta ou baixa '))
+m2=str(input('Você é loira ou morena '))
+
+if m1 == "Alta":
+    print("Você é uma mulher alta ")
+else:
+    print("Você é uma mulher baixa ")
+if m2 == "Loira":
+    print("Você é uma mulher loira ")
+else:
+    print("Você é uma mulher morena ")
+
+    
+   
+    
---
+++ funcao.py
@@ -0,0 +1,15 @@
+#funções 164 pagina, pre definição uma atividade ou rotina dentro do python, input
+#ou if else...
+
+#print vem com uma mensagem de texto e ele auto executa por tela...
+##def mensagem():
+##    print("Sejam bem vindo a PROZ!")
+#
+##mensagem()
+
+
+
+
+
+
+
